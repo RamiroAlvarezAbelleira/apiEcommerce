ef96341a154a4063e1841597eea108e5bd883c27
RamiroAlvarezAbelleira <alvarezramiro1997@gmail.com>
2023-03-18 17:29:53 -0300

    test

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -2,12 +2,12 @@ require('dotenv').config()
 module.exports = {
   
   "development": {
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  dialect: process.env.DB_DIALECT
+  username: process.env.DB_USERNAME || 'root',
+  password: process.env.DB_PASSWORD || 'HIOUNYMGOERdxfMrgJmA',
+  database: process.env.DB_DATABASE || 'railway',
+  host: process.env.DB_HOST || 'containers-us-west-26.railway.app',
+  port: process.env.DB_PORT || '5832',
+  dialect: process.env.DB_DIALECT || 'mysql'
   }
 
 }
