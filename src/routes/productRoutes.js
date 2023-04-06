78bb2d4b4efc95fd7f0c039bf8a6ff51bdd75103
RamiroAlvarezAbelleira <alvarezramiro1997@gmail.com>
2023-04-06 13:19:14 -0300

    added a new product edition validation and changed the names of the productDetailInfo response

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -4,6 +4,7 @@ const productController = require('../controllers/productController');
 const multerMW = require('../middleware/multerMW');
 const upload = multerMW('products', 'product-');
 const productValidation = require('../middleware/productValidatorMW');
+const productEditValidation = require('../middleware/productEditValidatorMW');
 
 /*RUTAS*/
 router.get('/', productController.list);
@@ -12,7 +13,7 @@ router.get('/info-formulario', productController.formInfo);
 router.post('/crear', upload.array('image'), productValidation, productController.create);
 router.get('/detalle/:id', productController.productDetail);
 router.get('/detalle-info/:id', productController.productDetailInfo);
-router.put('/editar/:id', upload.array('image'), productValidation, productController.edit);
+router.put('/editar/:id', upload.array('image'), productEditValidation, productController.edit);
 router.delete('/eliminar/:id', productController.delete);
 
 module.exports = router;
