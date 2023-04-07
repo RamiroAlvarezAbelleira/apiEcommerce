dfe2f6ce5b4f702d504e386b0b4bbe3214257112
RamiroAlvarezAbelleira <alvarezramiro1997@gmail.com>
2023-04-07 18:57:30 -0300

    railway test

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 module.exports = {
-  development: {
+  production: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
